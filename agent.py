6b286f75e02505c3d2293472badc86ca6984d62d
MinJune Yi <matia2.yi@gmail.com>
2021-08-28 00:04:55 +0900

    feat: add init in agent.py

--- agent.py
+++ agent.py
@@ -19,3 +19,36 @@ class Agent:
     ACTIONS = [ACTION_BUY, ACTION_SELL]
     NUM_ACTIONS = len(ACTIONS) # 인공 신경망에서 고려할 출력값의 개수
 
+    def __init__(self , environment, min_trading_unit = 1 , max_trading_unit = 2, delayed_reward_threshold = .05):
+        # Environment 객체
+        # 현재 주식 가격을 가져오기 위해 환경 참조
+        self.environment = environment
+
+        #최소 매매 단위, 최대 매매 단위
+        self.min_trading_unit = min_trading_unit # 최소 단일 거래 단위
+        self.max_trading_unit = max_trading_unit # 최대 단일 거래 단위
+
+        #지연보상 임계치
+        self.delayed_reward_threshold = delayed_reward_threshold
+
+        #Agent 클래스의 속성
+        self.initial_balance = 0 # 초기 자본금
+        self.balance = 0 # 현재 현금 잔고
+        self.num_stocks = 0 # 보유 주식 수
+        # PV = balance + num_stocks * {현재 주식 가격}
+        self.portfolio_value = 0
+        self.base_portfolio_value = 0 # 직전 학습 시점의 PV
+        self.num_buy = 0 # 매수 횟수
+        self.num_sell = 0 # 매도 횟수
+        self.num_hold = 0 # 홀딩 횟수
+        self.immediate_reward = 0 #즉시 보상
+        self.profitloss = 0 # 현재 손익
+        self.base_profitloss = 0 # 직전 지연 보상 이후 손익
+        self.exploration_base = 0 # 탐험 행동 결정 기준
+
+        # Agent 클래스의 상태
+        self.ratio_hold = 0 # 주식 보유 비율
+        self.ratio_portfolio_value = 0 # 포트폴리오 가치 비율
+
+
+
